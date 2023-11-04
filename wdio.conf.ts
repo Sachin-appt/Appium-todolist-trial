0ab401106e8879ea2cfd9aa6097e2500da7c3f54
Sachin-appt <sachin.datir@appt.dev>
2023-11-04 14:25:32 +0530

    test: setup allure report

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,6 +1,7 @@
 import type {Options} from '@wdio/types';
 import path from 'path';
-
+// import allureReporter from '@wdio/allure-reporter';
+import allure from 'allure-commandline';
 export const config: Options.Testrunner = {
   //
   // ====================
@@ -20,7 +21,8 @@ export const config: Options.Testrunner = {
   // ==================
   // Specify Test Files
   // ==================
-  // Define which test specs should run. The pattern is relative to the directory
+  // Define which test specs should run. The pattern is relative to the directoryimport allure from 'allure-commandline';
+
   // of the configuration file being run.
   //
   // The specs are defined as an array of spec files (optionally using wildcards
@@ -155,7 +157,17 @@ export const config: Options.Testrunner = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ['spec'],
+  reporters: [
+    'spec',
+    [
+      'allure',
+      {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+      },
+    ],
+  ],
 
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
@@ -301,8 +313,24 @@ export const config: Options.Testrunner = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {<Object>} results object containing test results
    */
-  // onComplete: function(exitCode, config, capabilities, results) {
-  // },
+  onComplete: function () {
+    const reportError = new Error('Could not generate Allure report');
+    const generation = allure(['generate', 'allure-results', '--clean']);
+    return new Promise<void>((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+
+      generation.on('exit', function (exitCode: number) {
+        clearTimeout(generationTimeout);
+
+        if (exitCode !== 0) {
+          return reject(reportError);
+        }
+
+        console.log('Allure report successfully generated');
+        resolve();
+      });
+    });
+  },
   /**
    * Gets executed when a refresh happens.
    * @param {string} oldSessionId session ID of the old session
